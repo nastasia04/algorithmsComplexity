691ac980eb38076694daa15925dc59c94aaee0ed
anastasia-alimpyeva <nastasia04@icloud.com>
2019-09-23 01:19:42 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -34,12 +34,6 @@ public class Main {
                 System.out.println("new branch main");
 
 
-                System.out.println("this is branch hello)");
-
-                System.out.println("new branch main section");
-                System.out.println("new branch main");
-                System.out.println("new branch main section");
-                System.out.println("new branch main");
                 count--;
             }
             count = NUMBER_OF_RUNS;
