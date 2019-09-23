74503ac249c1bc3e22c19422f470eb2135a0593d
anastasia-alimpyeva <nastasia04@icloud.com>
2019-09-23 07:27:24 +0300

    blahello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,10 +30,13 @@ public class Main {
                 System.out.println("master");
 
 
+                System.out.println("hello");
+
+
                 System.out.println("new branch main section");
                 System.out.println("new branch main");
 
-                System.out.println("hi");
+
 
                 count--;
             }
